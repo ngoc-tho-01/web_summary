4f8f889e0f5d68378367e367a2fcae4ae75c8f41
Đặng Ngọc Thọ <121676567+ngoc-tho-01@users.noreply.github.com>
2023-06-19 07:41:10 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+"use strict";
+//khai báo biến
+const biến = "giá trị";
+console.log(biến);
+
+// //hàm
+function bien(giatri) {
+  return 47 - giatri;
+}
+console.log(bien(40));
+
+//biểu thức hàm
+const tênBiến = function (giátrịgán) {
+  return 200 + giátrịgán;
+};
+console.log(tênBiến(50));
+
+//Hàm mũi tên
+const tenBien = (giatri) => 234 - giatri;
+console.log(tenBien(123));
